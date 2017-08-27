30d11a1ce5f2b220ee82f9ffae8e4f234e60152a
evgen.fesuk <evgen.fesuk@gmail.com>
2017-08-27 18:52:11 +0300

    умножение done

--- 3.11/3.11.cpp
+++ 3.11/3.11.cpp
@@ -1,6 +1,3 @@
-// 3.11.cpp: îïðåäåëÿåò òî÷êó âõîäà äëÿ êîíñîëüíîãî ïðèëîæåíèÿ.
-//
-
 #include "stdafx.h"
 #include <iostream>
 
@@ -10,11 +7,11 @@ int main()
 {
 	setlocale(LC_ALL, "rus");
 
-	cout << "Введите первое число: ";
+	cout << "Input first number: ";
 	int f1, sh1, p1;
 	cin >> f1 >> sh1 >> p1;
 
-	cout << "Выберите желаемую операцию (+, -, *, /): ";
+	cout << "What kind of operation do you need? (+, -, *, /): ";
 	char answer;
 	cin >> answer;
 
@@ -22,35 +19,50 @@ int main()
 	{
 	case '+':
 	{
-		cout << "Введите второе число: ";
+		cout << "Input second number: ";
 		int f2, sh2, p2;
 		cin >> f2 >> sh2 >> p2;
 		break;
 	}
 	case '-':
 	{
-		cout << "Введите второе число: ";
+		cout << "Input second number: ";
 		int f2, sh2, p2;
 		cin >> f2 >> sh2 >> p2;
 		break;
 	}
 	case '*':
 	{
-		cout << "Введите множитель: ";
+		cout << "Input factor: ";
 		int factor;
 		cin >> factor;
+		int newF, newSh, newP;
+		newF = f1 * factor;
+		newSh = sh1 * factor;
+		newP = p1 * factor;
+		if (newP >= 12)
+		{
+			newSh++;
+			newP -= 12;
+		}
+		if (newSh >= 20)
+		{
+			newF++;
+			newSh -= 20;
+		}
+		cout << newF << " pounds " << newSh << " shillings " << newP << " pence." << endl;
 		break;
 	}
 	case '/':
 	{
-		cout << "Введите делитель: ";
+		cout << "Input divider: ";
 		int divider;
 		cin >> divider;
 		break;
 	}
 
 	default:
-		cout << "Неверный формат ввода" << endl;
+		cout << "Wrong choise" << endl;
 		break;
 	}
 
